8d69f2e5ebb1496fdb053166f6e0a2cde28e8644
iNKidzy <nadq.nadia@gmail.com>
2021-08-31 09:33:12 +0200

    fix FormGroup in new post html

--- apps/postr/src/app/new-post/new-post.component.ts
+++ apps/postr/src/app/new-post/new-post.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
+type Post = {
+  id: 1;
+  author: string;
+  title: string;
+  body: string;
+}
 @Component({
   selector: 'graphql-state-spike-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.scss']
 })
-
 export class NewPostComponent implements OnInit{
+  readonly form = this.fb.group({
+    title: ['', Validators.required],
+    author: ['', Validators.required],
+    body: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder){}
+  
+  
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
- 
+
+  async onSubmit(){
+    console.log
+  }
+
 }

2702b96cc9c85d847609a6dcda2ba5d6c9de69da
iNKidzy <nadq.nadia@gmail.com>
2021-09-01 16:01:03 +0200

    Test 2 + ALL PASS

--- apps/postr/src/app/new-post/new-post.component.spec.ts
+++ apps/postr/src/app/new-post/new-post.component.spec.ts
@@ -1,16 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NewPostComponent } from './new-post.component';
-import { ApolloTestingModule } from 'apollo-angular/testing';
+import {
+  ApolloTestingController,
+  ApolloTestingModule,
+} from 'apollo-angular/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
+import { AddPostDocument } from '@graphql-state-spike/data-access';
 
 describe('NewPostComponent', () => {
   let component: NewPostComponent;
   let fixture: ComponentFixture<NewPostComponent>;
+  let controller: ApolloTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -31,6 +36,12 @@ describe('NewPostComponent', () => {
     fixture = TestBed.createComponent(NewPostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    controller = TestBed.inject(ApolloTestingController);
+  });
+
+  afterEach(() => {
+    controller.verify();
   });
 
   it('should create', () => {
@@ -43,12 +54,39 @@ describe('NewPostComponent', () => {
       component.form.get('body')?.setValue('Body');
       component.onSubmit();
 
-    }),
- // }),
- /* describe('Form submit', () => {
-    it('should make a new post', () => {
-      
+      const op = controller.expectOne(AddPostDocument);
+      expect(op.operation.variables).toEqual({
+        title: 'Title',
+        body: 'Body',
+        authorId: 1,
+      });
+    });
+  });
 
-    }),
-  }), */
-//}); 
+  describe('Form submit', () => {
+    it('should make a new post', () => {
+      setValue(element<HTMLInputElement>('#titleInput'), 'Title');
+      setValue(element<HTMLTextAreaElement>('#bodyInput'), 'Body');
+      click(element<HTMLButtonElement>('#submit'));
+
+      const op = controller.expectOne(AddPostDocument);
+      expect(op.operation.variables).toEqual({
+        title: 'Title',
+        body: 'Body',
+        authorId: 1,
+      });
+    });
+  });
+
+  function element<T extends HTMLElement>(selector: string) {
+    return fixture.nativeElement.querySelector(selector) as T;
+  }
+
+  function setValue(elemnt: HTMLElement & { value: any }, value: string) {
+    elemnt.value = value;
+    elemnt.dispatchEvent(new Event('input'));
+  }
+  function click(elemnt: HTMLElement) {
+    elemnt.dispatchEvent(new Event('click'));
+  }
+});

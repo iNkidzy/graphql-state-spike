3f4da29c939e85a4dc0e9ea0439c23925a602501
iNKidzy <nadq.nadia@gmail.com>
2021-09-01 15:16:20 +0200

    test 1 FormCheck

--- apps/postr/src/app/new-post/new-post.component.spec.ts
+++ apps/postr/src/app/new-post/new-post.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NewPostComponent } from './new-post.component';
 import { ApolloTestingModule } from 'apollo-angular/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
 
 describe('NewPostComponent', () => {
   let component: NewPostComponent;
@@ -8,10 +14,17 @@ describe('NewPostComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ApolloTestingModule],
-      declarations: [ NewPostComponent ]
-    })
-    .compileComponents();
+      imports: [
+        ApolloTestingModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        MatInputModule,
+        NoopAnimationsModule,
+        RouterTestingModule,
+      ],
+      declarations: [NewPostComponent],
+      providers: [{ provide: MATERIAL_SANITY_CHECKS, useValue: false }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -23,4 +36,19 @@ describe('NewPostComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  describe('Form', () => {
+    it('should invoke addPost mutation ', () => {
+      component.form.get('title')?.setValue('Title');
+      component.form.get('body')?.setValue('Body');
+      component.onSubmit();
+
+    }),
+ // }),
+ /* describe('Form submit', () => {
+    it('should make a new post', () => {
+      
+
+    }),
+  }), */
+//}); 
